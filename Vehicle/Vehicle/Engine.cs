217707d5342d31ea2fb9689afd0b53dbb8b92006
William Long <williamlong@outlook.com>
2016-06-02 15:15:29 -0700

    framework

---
+++ Vehicle/Vehicle/Engine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class Engine
+    {
+		#region properties
+
+	    public int Temperature { get; set; }
+	    public int InstantaneousVelocity { get; set; }
+	    public int FuelLevel { get; set; }
+	    public bool IsRunning { get; set; }
+		
+		#endregion
+
+		#region public methods
+
+	    public void Start()
+	    {
+		    
+	    }
+
+	    public void Stop()
+	    {
+		    
+	    }
+
+	    public void Accelerate()
+	    {
+		    
+	    }
+
+	    public void Decelerate()
+	    {
+		    
+	    }
+		
+		#endregion	
+
+
+
+	}
+}
